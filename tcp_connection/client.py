e620c8c0ce4478c19ceeafdadccd826a52211a89
Ramla-I <ijazramla@gmail.com>
2021-06-16 22:58:30 -0500

    working async client server

--- tcp_connection/client.py
+++ tcp_connection/client.py
@@ -1,6 +1,6 @@
 import socket
 
-hostname, sld, tld, port = 'www', 'integralist', 'co.uk', 80
+hostname, sld, tld, port = 'www', 'integralist', 'co.uk', 8881
 target = '{}.{}.{}'.format(hostname, sld, tld)
 
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
@@ -8,7 +8,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the client
 # client.connect((target, port))
-client.connect(('0.0.0.0', 9999))
+client.connect(('0.0.0.0', port))
 
 # send some data (in this case a HTTP GET request)
 client.send('GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld))
@@ -16,4 +16,4 @@ client.send('GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld))
 # receive the response data (4096 is recommended buffer size)
 response = client.recv(4096)
 
-print response
+print(response)
